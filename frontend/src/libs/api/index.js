dad993b40de1586569207c6fbecfb8bf93755938
render <142716427+render217@users.noreply.github.com>
2024-08-27 00:05:35 +0300

    fix typo BASE_URL

--- frontend/src/libs/api/index.js
+++ frontend/src/libs/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import Cookies from "js-cookie";
 
-const BASE_URL = import.meta.VITE_BACKEND_URL;
+const BASE_URL = import.meta.env.VITE_BACKEND_URL;
 // const BASE_URL = "http://localhost:3500/api/v1";
 
 const API = axios.create({
